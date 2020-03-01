2a0d348d649bb0b44267185a8dc78c85fc42ab29
Andree Avalos <aavalosoto@gmail.com>
2020-02-29 20:21:30 -0600

    Conexion

---
+++ Serverless_Pattern/Helpers/Conexion.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serverless_Pattern.Helpers
+{
+    class Conexion
+    {
+        MySqlConnection conex = new MySqlConnection();
+
+        static string server = "Server=35.188.13.111:3306;";
+        static string db = "Database=DB_PazHUB;";
+        static string usuario = "Uid=root;";
+        static string password = "pwd=root;";
+        public string result = "";
+        string CadenaDeConexion = server + db + usuario + password;
+
+        public Conexion() {
+            try
+            {
+                conex.ConnectionString = CadenaDeConexion;
+                conex.Open();
+                result = "Exito";
+            }
+            catch (MySqlException)
+            {
+
+                result = "Fracaso";
+            }
+        }
+
+
+    }
+}
